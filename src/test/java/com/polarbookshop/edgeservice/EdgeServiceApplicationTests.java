3818c223bf8d5352b01105d8f3899314dc1d0463
Qu Changyu <qchy_dev@outlook.com>
2026-01-05 00:29:54 +0800

    09 end

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,12 +2,34 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class EdgeServiceApplicationTests {
 
+    private static final int REDIS_PORT = 6379;
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>(
+            DockerImageName.parse("redis:7.2"))
+            .withExposedPorts(REDIS_PORT);
+
+    @DynamicPropertySource // 重写Redis配置，以指向用于测试的Redis实例
+    static void redisProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.redis.host", () -> redis.getHost());
+        registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
+    }
+
     @Test
-    void contextLoads() {
+    void verifyThatSpringContextLoads() { // 用于验证应用上下文能够正确加载以及到Redis的连接能够成功创建的空测试方法
     }
 
 }
